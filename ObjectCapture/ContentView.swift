9e54560c19b1046295f7eafcee885a94c4eda900
Jay Jiang <jayjiang@xm.wonder.com>
2024-03-22 04:50:30 -0400

    Initial project
    - Init the app project

---
+++ ObjectCapture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectCapture
+//
+//  Created by jayjiang on 22/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectCapture/ObjectCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectCaptureApp.swift
+//  ObjectCapture
+//
+//  Created by jayjiang on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObjectCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
